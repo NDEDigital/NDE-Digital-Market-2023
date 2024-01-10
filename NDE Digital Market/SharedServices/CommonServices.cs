5b91e3292eecca2acaccd2eeecd0fb7dacf2935d
Helena Akter Reya <helena.aktar@ndessl.local>
2024-01-10 18:13:06 +0600

    image compressed

--- NDE Digital Market/SharedServices/CommonServices.cs
+++ NDE Digital Market/SharedServices/CommonServices.cs
@@ -2,7 +2,6 @@
 using NDE_Digital_Market.Model.MaterialStock;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Jpeg;
-using SixLabors.ImageSharp.Formats.Png;
 using SixLabors.ImageSharp.Processing;
 using System.Data.SqlClient;
 using System.Security.Cryptography;
