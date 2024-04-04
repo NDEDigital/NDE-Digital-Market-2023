eb186fef3e6f5ad5a4501e7f1ce26f58350752de
Mahfuzul Alam <mahfuzul@ndessl.local>
2024-04-04 12:17:43 +0600

    top seller to details page

---
+++ NDE Digital Market/Model/TopSellerModel.cs
@@ -0,0 +1,13 @@
+﻿namespace NDE_Digital_Market.Model
+{
+    public class TopSellerModel
+    {
+        public string CompanyCode { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyImage { get; set; }
+        public int ProductGroupID { get; set; }
+        public int TotalQty { get; set; }
+        public string ProductGroupCode { get; set; }
+
+    }
+}
