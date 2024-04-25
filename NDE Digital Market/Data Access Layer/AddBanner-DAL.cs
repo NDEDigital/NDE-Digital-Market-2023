2e995b2c7c0ffaabd57960cf43e2149bc4354a64
NDEDigital <ndedigitalmarket@gmail.com>
2024-04-25 12:42:37 +0600

    new coll added

--- NDE Digital Market/Data Access Layer/AddBanner-DAL.cs
+++ NDE Digital Market/Data Access Layer/AddBanner-DAL.cs
@@ -161,21 +161,21 @@ namespace NDE_Digital_Market.Data_Access_Layer
             string query = string.Empty;
             if (status is null)
             {
-                query = @"    SELECT AB.BannerID, AB.BannerDescription, AB.BannerImage, CR.CompanyName
-                              FROM AdBanner AB
-                              join CompanyRegistration CR on CR.CompanyCode = Ab.CompanyCode
-                              where AB.IsBannerStatus is null;";
+                query = @"SELECT AB.BannerID, AB.BannerDescription, AB.BannerImage, CR.CompanyName, AB.StartDate, AB.EndDate, AB.AddedDate
+                        FROM AdBanner AB
+                        join CompanyRegistration CR on CR.CompanyCode = Ab.CompanyCode
+                        where AB.IsBannerStatus is null;";
             }
             else if (status == true)
             {
-                query = @"    SELECT AB.BannerID, AB.BannerDescription, AB.BannerImage, CR.CompanyName
+                query = @"    SELECT AB.BannerID, AB.BannerDescription, AB.BannerImage, CR.CompanyName, AB.StartDate, AB.EndDate, AB.AddedDate
                               FROM AdBanner AB
                               join CompanyRegistration CR on CR.CompanyCode = Ab.CompanyCode
                               where AB.IsBannerStatus = 'true';";
             }
             else if (status == false)
             {
-                query = @"    SELECT AB.BannerID, AB.BannerDescription, AB.BannerImage, CR.CompanyName
+                query = @"    SELECT AB.BannerID, AB.BannerDescription, AB.BannerImage, CR.CompanyName, AB.StartDate, AB.EndDate, AB.AddedDate
                               FROM AdBanner AB
                               join CompanyRegistration CR on CR.CompanyCode = Ab.CompanyCode
                               where AB.IsBannerStatus = 'false';";
@@ -196,6 +196,36 @@ namespace NDE_Digital_Market.Data_Access_Layer
                 banner.BannerImage = reader["BannerImage"].ToString();
                 banner.CompanyName = reader["CompanyName"].ToString();
 
+                //banner.StartDate =Convert.ToDateTime(reader["StartDate"]);
+                //banner.EndDate =Convert.ToDateTime(reader["EndDate"]);
+
+                if (!reader.IsDBNull(reader.GetOrdinal("AddedDate")))
+                {
+                    banner.AddedDate = Convert.ToDateTime(reader["AddedDate"]);
+                }
+                else
+                {
+                    // Handle the case when StartDate is DBNull
+                    banner.AddedDate = null;
+                }
+                if (!reader.IsDBNull(reader.GetOrdinal("StartDate")))
+                {
+                    banner.StartDate = Convert.ToDateTime(reader["StartDate"]);
+                }
+                else
+                {
+                    // Handle the case when StartDate is DBNull
+                    banner.StartDate = null;
+                }
+                if (!reader.IsDBNull(reader.GetOrdinal("EndDate")))
+                {
+                    banner.EndDate = Convert.ToDateTime(reader["EndDate"]);
+                }
+                else
+                {
+                    // Handle the case when StartDate is DBNull
+                    banner.EndDate = null;
+                }
 
                 banners.Add(banner);
             }
