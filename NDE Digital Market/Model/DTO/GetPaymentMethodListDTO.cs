a2f2ae602c94317427e751b60280b4e5dbe8e341
NDEDigital <ndedigitalmarket@gmail.com>
2024-06-02 13:16:04 +0600

    HK_gets modified

---
+++ NDE Digital Market/Model/DTO/GetPaymentMethodListDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace NDE_Digital_Market.Model.DTO
+{
+    public class GetPaymentMethodListDTO
+    {
+        public string PMID { get; set; }
+        public string PMName { get; set; }
+    }
+}
--- NDE Digital Market/Services/HK_GetsServices/IHK_Gets.cs
+++ NDE Digital Market/Services/HK_GetsServices/IHK_Gets.cs
@@ -1,10 +1,12 @@
 ﻿using NDE_Digital_Market.Model;
+using NDE_Digital_Market.Model.DTO;
 
 namespace NDE_Digital_Market.Services.HK_GetsServices
 {
     public interface IHK_Gets
     {
-        Task<List<PaymentMethodModel>> PaymentMethodGetAsync();
-        Task<List<PaymentMethodModel>> BankNameGetAsync(int preferredPM);
+        Task<List<GetPaymentMethodListDTO>> PaymentMethodGetAsync();
+        Task<List<GetPaymentMethodListDTO>> BankNameGetAsync(string preferredPM);
+        Task<List<GetReturnTypeListDTO>> GetReturnListAsync();
     }
 }
