de45c1e3bbfb6ef66a9b73ac3c15309693d60316
NDEDigital <ndedigitalmarket@gmail.com>
2024-07-08 11:18:33 +0600

    query change 2

--- NDE Digital Market/Controllers/GoodsController.cs
+++ NDE Digital Market/Controllers/GoodsController.cs
@@ -139,10 +139,7 @@ namespace NDE_Digital_Market.Controllers
             try
             {
 
-                if (ProductGroupCode == null)
-                {
-                    return NotFound(new { message = "Give Valid Data." });
-                }
+
                 object res = await _Goods_Service.GetProductList(CompanyCode, ProductGroupCode);
                 if (res == null)
                 {
